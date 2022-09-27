e99653446805c48f95536b9fcabe567a786a1006
nham.dinh <nhamnd.hmu@gmail.com>
2022-09-28 00:33:06 +0700

    connect to DB

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,1 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("welcome to homepage");
+});
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});
