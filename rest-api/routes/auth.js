18fcacb826de9fb3c0287f777e0d469e6a82bb47
nham.dinh <nhamnd.hmu@gmail.com>
2022-09-29 16:12:14 +0700

    finish login register auth

--- rest-api/routes/auth.js
+++ rest-api/routes/auth.js
@@ -21,11 +21,29 @@ router.get("/register", async (req, res) => {
             password: hashedPassword,
         });
 
-        //save user and respond
+        //save user and return respond
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        !validPassword && res.status(400).json("wrong password");
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
