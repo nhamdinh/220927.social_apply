f0256f8fbefcc50fd2cb85a5543874b5a2577f7d
nham.dinh <nhamnd.hmu@gmail.com>
2022-09-29 15:40:37 +0700

    create routes

--- rest-api/index.js
+++ rest-api/index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
 const app = express();
 dotenv.config();
 
@@ -23,6 +26,13 @@ app.use(morgan("common"));
 app.get("/", (req, res) => {
     res.send("welcome to homepage");
 });
+
+app.get("/users", (req, res) => {
+    res.send("welcome to users");
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
