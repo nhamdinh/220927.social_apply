7e6fedbb9ddc2a0642f6007faef531cc16a0c33b
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-03 19:19:32 +0700

    create home page

---
+++ front-end/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "./../../components/topbar/TopBar";
+import styles from "./home.module.scss";
+
+import { CircularProgress } from "@material-ui/core";
+
+export default function Home() {
+    return (
+        <div>
+            <TopBar> </TopBar>
+        </div>
+    );
+}
