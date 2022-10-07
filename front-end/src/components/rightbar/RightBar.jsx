ae23f6592276629be6bcac43bcd040896249f6f6
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-07 21:34:57 +0700

    create rightbar/ sidebar/

---
+++ front-end/src/components/rightbar/RightBar.jsx
@@ -0,0 +1,5 @@
+import styles from "./rightBar.module.scss";
+
+export default function RightBar() {
+    return <div>rrr</div>;
+}
---
+++ front-end/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,5 @@
+import styles from "./sideBar.module.scss";
+
+export default function SideBar() {
+    return <div>ss</div>;
+}
