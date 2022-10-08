934690a829ac0b0959da0815e8998ded6458f309
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-08 16:35:58 +0700

    create backend server images by multer path lib

--- rest-api/index.js
+++ rest-api/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -18,10 +20,23 @@ mongoose.connect(
     }
 );
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
     res.send("welcome to homepage");
