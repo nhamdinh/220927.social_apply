ad6adcc69d40698797d23666acaf3e06a777dfe9
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-09 19:07:03 +0700

    add react-router-dom

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import "./scss_common/reset.css";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="profile/:username" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
