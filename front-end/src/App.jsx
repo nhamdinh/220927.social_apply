289ff9c87b4ad4bf867a6d331b2e0e79229b3f3d
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-12 17:49:57 +0700

    finish page  register/ v1.0

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,14 +4,24 @@ import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 export default function App() {
+    const { user } = useContext(AuthContext);
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
+                <Route path="/" element={user ? <Home /> : <Login />} />
+                <Route
+                    path="login"
+                    element={user ? <Navigate to="/" /> : <Login />}
+                />
+                <Route
+                    path="register"
+                    element={user ? <Navigate to="/" /> : <Register />}
+                />
                 <Route path="profile/:username" element={<Profile />} />
             </Routes>
         </BrowserRouter>
---
+++ front-end/src/pages/register/Register.jsx
@@ -0,0 +1,108 @@
+import styles from "./register.module.scss";
+import { useRef, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { registerCall } from "../../apiCalls";
+
+export default function Register() {
+    const username = useRef();
+    const email = useRef();
+    const password = useRef();
+    const passwordAgain = useRef();
+    const navigate = useNavigate();
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        if (password.current.value !== passwordAgain.current.value) {
+            passwordAgain.current.setCustomValidity("Passwords don't match!");
+        } else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value,
+            };
+            registerCall(user);
+            navigate("/login");
+        }
+    };
+    return (
+        <div className={styles.registerContainer}>
+            <div className={`${styles.grid__1200} ${styles.wide}`}>
+                <div className={`${styles.grid__row} `}>
+                    <div
+                        className={`${styles.grid__col} ${styles.l__6} ${styles.m__5} ${styles.s__12}`}
+                    >
+                        <div className={styles.register__left}>
+                            <div className={styles.register__left__logo}>
+                                bonjour social
+                            </div>
+                            <span className={styles.register__left__slogan}>
+                                Connect with friends and the world around you on
+                                bonjour social.
+                            </span>
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.grid__col} ${styles.l__5} ${styles.m__5} ${styles.s__12}`}
+                    >
+                        <form
+                            onSubmit={handleClick}
+                            className={styles.register__right}
+                        >
+                            <ul className={styles.right__input__list}>
+                                <li>
+                                    <input
+                                        required
+                                        type="text"
+                                        className={styles.right__input__item}
+                                        placeholder="Username"
+                                        ref={username}
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        required
+                                        type="text"
+                                        className={styles.right__input__item}
+                                        placeholder="Email"
+                                        ref={email}
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        required
+                                        type="password"
+                                        className={styles.right__input__item}
+                                        placeholder="Password"
+                                        ref={password}
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        required
+                                        type="password"
+                                        className={styles.right__input__item}
+                                        placeholder="Re-Password"
+                                        ref={passwordAgain}
+                                        minLength={3}
+                                    />
+                                </li>
+                                <button
+                                    type="submit"
+                                    className={styles.right__input__signUp}
+                                >
+                                    sign Up
+                                </button>
+                                <Link
+                                    to="/login"
+                                    className={styles.right__input__signIn}
+                                >
+                                    Login to account
+                                </Link>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
