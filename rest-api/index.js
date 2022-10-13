bb2d0912cd69d1ae8f4f307c21a716500582f505
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-13 10:36:04 +0700

    api  posts

--- rest-api/index.js
+++ rest-api/index.js
@@ -9,6 +9,7 @@ const path = require("path");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const app = express();
 app.use(
@@ -70,6 +71,7 @@ app.get("/posts", (req, res) => {
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
