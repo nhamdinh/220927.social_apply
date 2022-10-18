dd4e5e172f63e408bc351071ae2819b81efe392c
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-18 19:08:35 +0700

    update preview post image

--- front-end/src/components/post/Post.jsx
+++ front-end/src/components/post/Post.jsx
@@ -1,7 +1,6 @@
 import styles from "./post.module.scss";
 import { AuthContext } from "./../../context/AuthContext";
-import { useContext, useState, useRef, useEffect } from "react";
-
+import { useContext, useState, useRef } from "react";
 import { uploadCall, postsCall } from "./../../apiCalls";
 import { IMG_NO_AVATAR } from "./../../const";
 import {
@@ -10,26 +9,12 @@ import {
     Room,
     EmojiEmotions,
     Cancel,
-    CallMerge,
 } from "@material-ui/icons";
 export default function Post() {
     const PF = process.env.REACT_APP_USERS_FOLDER;
     const { user } = useContext(AuthContext);
     const [file, setFile] = useState(null);
-    const [preview, setPreview] = useState(null);
     const descRef = useRef();
-    const fileInputRef = useRef();
-    useEffect(() => {
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setPreview(String(reader.result));
-            };
-            reader.readAsDataURL(file);
-        } else {
-            setPreview(null);
-        }
-    }, [file]);
     const submitHandler = async (e) => {
         e.preventDefault();
         const newPost = { userId: user._id, desc: descRef.current.value };
@@ -48,7 +33,9 @@ export default function Post() {
         try {
             postsCall(newPost);
             alert("The post has been upload success!!");
-            window.location.reload();
+            /*   
+                    window.location.reload();
+             */
         } catch (err) {
             console.log(err);
         }
@@ -58,7 +45,7 @@ export default function Post() {
             <div className={styles.post__top}>
                 <img
                     src={user ? PF + user.profilePicture : PF + IMG_NO_AVATAR}
-                    alt=""
+                    alt={IMG_NO_AVATAR}
                     className={styles.top__avatar}
                 />
 
@@ -69,27 +56,29 @@ export default function Post() {
                     className={styles.top__input}
                 />
             </div>
-            <img
-                src={preview}
-                alt="preview"
-                className={styles.post__preview}
-                style={{ display: { preview } == null ? "none" : "block" }}
-            />
+            {file !== null && (
+                <div className={styles.post__img}>
+                    <Cancel
+                        className={styles.post__CancelImg}
+                        onClick={() => setFile(null)}
+                    />
+                    <img
+                        src={URL.createObjectURL(file)}
+                        alt="preview"
+                        className={styles.post__preview}
+                    />
+                </div>
+            )}
             <hr className={styles.post__hr} />
             <form onSubmit={submitHandler} className={styles.post__bottom}>
                 <ul className={styles.bottom__list}>
                     <label htmlFor="file" className={styles.bottom__item}>
                         <PermMedia
-                            onClick={(event) => {
-                                event.preventDefault();
-                                fileInputRef.current.click();
-                            }}
                             htmlColor="tomato"
                             className={styles.item__icon}
                         ></PermMedia>
                         <div className={styles.item__title}>photo or video</div>
                         <input
-                            ref={fileInputRef}
                             type="file"
                             id="file"
                             accept=".png,.jpeg,.jpg"
