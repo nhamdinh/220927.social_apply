d741e7ee6bd6421eb2b6a6b99eca0f0735c9cf15
nham.dinh <nhamnd.hmu@gmail.com>
2022-10-20 15:11:52 +0700

    updated follow/un follow

--- front-end/src/components/rightbar/RightBar.jsx
+++ front-end/src/components/rightbar/RightBar.jsx
@@ -1,7 +1,7 @@
 import styles from "./rightBar.module.scss";
 import { Add, Remove } from "@material-ui/icons";
 import OnlineFriends from "./../onlineFriends/OnlineFriends";
-import { Users } from "./../../dummyData";
+import { UsersOnline } from "./../../dummyData";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -9,42 +9,93 @@ import { useParams } from "react-router";
 import { AuthContext } from "../../context/AuthContext";
 
 import { IMG_GIFT, IMG_AD, IMG_NO_AVATAR } from "./../../const";
-export default function RightBar({}) {
+export default function RightBar() {
     const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
     const USERS_FOLDER = process.env.REACT_APP_USERS_FOLDER;
     const usernameParams = useParams().username;
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
+    const [currentUser, setCurrentUser] = useState(user);
     const [friends, setFriends] = useState([]);
+    const [followed, setFollowed] = useState(null);
+
+    const handleClick = async () => {
+        try {
+            if (followed) {
+                await axios.put(`/users/${user._id}/unfollow`, {
+                    userId: currentUser._id,
+                });
+                /*                 dispatch({ type: "UNFOLLOW", payload: user._id });
+                 */ setFollowed(!followed);
+            } else {
+                await axios.put(`/users/${user._id}/follow`, {
+                    userId: currentUser._id,
+                });
+                /*                 dispatch({ type: "FOLLOW", payload: user._id });
+                 */ setFollowed(!followed);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
     useEffect(() => {
         const fetchFriendsList = async () => {
-            try {
-                if (usernameParams) {
-                    if (user._id) {
+            if (usernameParams) {
+                try {
+                    if (currentUser._id) {
                         const friendList = await axios.get(
-                            "/users/friends/" + user._id
+                            "/users/friends/" + currentUser._id
                         );
                         setFriends(friendList.data);
                     }
+                } catch (err) {
+                    console.log(err);
                 }
-            } catch (err) {
-                console.log(err);
             }
         };
         fetchFriendsList();
+        setFollowed([...currentUser.followings].includes(user._id));
+    }, [currentUser, user]);
+    useEffect(() => {
+        const fetchUser = async () => {
+            if (usernameParams) {
+                try {
+                    const res = await axios.get(
+                        `/users?username=${usernameParams}`
+                    );
+                    setCurrentUser(res.data);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        };
+
+        fetchUser();
     }, [usernameParams]);
     const ProfileRightBar = () => {
         return (
             <>
+                {user.username !== currentUser.username && (
+                    <button
+                        className={styles.rightBar__follow}
+                        onClick={handleClick}
+                    >
+                        {followed ? "Unfollow" : "Follow"}
+                        {followed ? <Remove /> : <Add />}
+                    </button>
+                )}
                 <div className={styles.rightBar__info}>
                     <div className={styles.rightBar__info__title}>
-                        your information
+                        {user.username === currentUser.username
+                            ? "your "
+                            : currentUser.username + "'s "}
+                        information
                     </div>
                     <div className={styles.rightBar__info__desc}>
                         <span className={styles.rightBar__info__key}>
                             city :
                         </span>
                         <span className={styles.rightBar__info__value}>
-                            {user.city}
+                            {currentUser.city}
                         </span>
                     </div>
                     <div className={styles.rightBar__info__desc}>
@@ -52,7 +103,7 @@ export default function RightBar({}) {
                             from :
                         </span>
                         <span className={styles.rightBar__info__value}>
-                            {user.from}
+                            {currentUser.from}
                         </span>
                     </div>
                     <div className={styles.rightBar__info__desc}>
@@ -60,9 +111,9 @@ export default function RightBar({}) {
                             relationship :
                         </span>
                         <span className={styles.rightBar__info__value}>
-                            {user.relationship == 1
+                            {currentUser.relationship === 1
                                 ? "Single"
-                                : user.relationship == 2
+                                : currentUser.relationship === 2
                                 ? "Married"
                                 : "-"}
                         </span>
@@ -70,13 +121,17 @@ export default function RightBar({}) {
                 </div>
                 <div className={styles.rightBar__following}>
                     <span className={styles.rightBar__following__title}>
-                        your friends list
+                        {user.username === currentUser.username
+                            ? "your "
+                            : currentUser.username + "'s "}
+                        friends list
                     </span>
                     <ul className={styles.rightBar__following__list}>
                         {friends.map((friend) => (
                             <Link
                                 key={friend._id}
                                 to={"/profile/" + friend.username}
+                                style={{ textDecoration: "none" }}
                             >
                                 <li
                                     className={styles.rightBar__following__item}
@@ -88,7 +143,7 @@ export default function RightBar({}) {
                                                   friend.profilePicture
                                                 : PUBLIC_FOLDER + IMG_NO_AVATAR
                                         }
-                                        alt=""
+                                        alt={IMG_NO_AVATAR}
                                         className={styles.following__item__img}
                                     />
                                     <span
@@ -114,8 +169,8 @@ export default function RightBar({}) {
                         className={styles.birthday__gift}
                     />
                     <span className={styles.birthday__text}>
-                        <b>Ngọc Trinh</b> and <b>3 other friends</b> have a
-                        birhday today.
+                        <strong>Ngọc Trinh</strong> and
+                        <strong>3 other friends</strong> have a birthday today.
                     </span>
                 </div>
                 <img
@@ -128,8 +183,11 @@ export default function RightBar({}) {
                         online friends
                     </div>
                     <ul className={styles.friendsOnline__list}>
-                        {Users.map((u) => (
-                            <OnlineFriends key={u.id} use={u}></OnlineFriends>
+                        {UsersOnline.map((user) => (
+                            <OnlineFriends
+                                key={user.id}
+                                use={user}
+                            ></OnlineFriends>
                         ))}
                     </ul>
                 </div>
